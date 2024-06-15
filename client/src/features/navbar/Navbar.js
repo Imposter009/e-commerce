e759fae34dfdd8825951c579462a2b5c0a5804f8
Sumit <sumitgupta19900@gmail.com>
2024-06-16 02:13:27 +0530

    this is the new build file

--- client/src/features/navbar/Navbar.js
+++ client/src/features/navbar/Navbar.js
@@ -95,7 +95,7 @@ function NavBar({ children }) {
                       {/* Profile dropdown */}
                       <Menu as="div" className="relative ml-3">
                         <div>
-                          <Menu.Button className="flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                          <Menu.Button className="flex max-w-xs items-center rounded-full bg-pink-200 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                             <span className="sr-only">Open user menu</span>
                             <img
                               className="h-8 w-8 rounded-full"
